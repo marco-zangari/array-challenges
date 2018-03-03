8ac4952e580ce7ce944f4ee63f5354a1a0acbab9
Marco Zangari <mjzangari@gmail.com>
2018-03-02 22:06:12 -0800

    added new challenge

---
+++ alternation.py
@@ -0,0 +1,12 @@
+"""An alternation is when 0 index is less than 1, 1 greater than 2 etc."""
+
+def alternation(arr):
+    """Alternate less and greater values.
+
+    input: array, ex input a[0], a[1], a[2]
+    output: array, with values alternated
+    ex output: b[0] <= b[1] >= b[2] <= b[3] >= b[4] <= b[5] etc.
+    """
+
+    a = a.sort()
+    return a
